4a0ca306a10ab385c6aa78ce05beb0d5f81524e1
uzairrizwan81 <uzair@neuralflex.com>
2025-12-05 14:16:20 +0500

    Phase 1 frontend: Set up Next.js application and clean up backend files

---
+++ frontend/.next/dev/static/chunks/frontend_app_page_tsx_23dee4cd._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/frontend_app_page_tsx_da8e6a03._.js",
+  "static/chunks/node_modules_d73dec3c._.js",
+  "static/chunks/frontend_styles_media-app_1cf92f3a.css"
+],
+    source: "dynamic"
+});
